5efa93649a9ecda4f1bdc5958462eca97d1b1b04
GiusiGrassia <36530384+GiusiGrassia@users.noreply.github.com>
2021-12-14 13:50:32 +0100

    Update stundenplan.js

--- Stundenplan/js/stundenplan.js
+++ Stundenplan/js/stundenplan.js
@@ -53,7 +53,7 @@ $(document).ready(function() {
         });
     };
 
-    // Change Handler Berufsgruppe
+    // Change Handler Klassenwahl
     $('#klassenauswahl').change(function(e) {
         console.log(this.value);
         // Stundenplan leeren
@@ -64,42 +64,38 @@ $(document).ready(function() {
 
     // Ajax Request Stundenplan
     function load_stundenplan(klasse_id) {
-        $('#klassenauswahl').change(function(e) {
-            // leeren der Ausgabe
-            $('#stundenplan').empty();
-            // Ajax Request
-            $.ajax({
-                type: "GET",
-                url: 'http://sandbox.gibm.ch/tafel.php?klasse_id=' + klasse_id,
-                data: { format: 'json', klasse: this.value }, // format und id mitgeben
-                dataType: 'json'
-            }).done(function(data) {
-                // wenn  Daten vorhanden sind ...
-                if (data != '') {
-                    // Tabelle generieren - Bootstrap Table
-                    $('#stundenplan').append('<table class="table"><tr><th>Datum</th><th>Wochentag</th><th>Von</th><th>Bis</th><th>Lehrer</th><th>Fach</th><th>Longfach</th><th>Raum</th><th>Kommentar</th></tr></table>');
-                    // Loop über JSON
-                    $.each(data, function(key, tafel) {
-                        // Tabellenzeilen anfügen   
-                        $('#stundenplan table').append('<tr><td>' + tafel.tafel_datum +
-                            '</td><td>' + tafel.tafel_wochentag +
-                            '</td><td>' + tafel.tafel_von +
-                            '</td><td>' + tafel.tafel_bis +
-                            '</td><td>' + tafel.tafel_lehrer +
-                            '</td><td>' + tafel.tafel_fach +
-                            '</td><td>' + tafel.tafel_longfach +
-                            '</td><td>' + tafel.tafel_raum +
-                            '</td><td>' + tafel.tafel_kommentar +
-                            '</td></tr>');
-                    })
-                } else {
-                    // Fehlermeldung ausgeben - Bootstrap alert Box
-                    $('#stundenplan').html('<div class="alert alert-warning">Wählen Sie eine Filiale aus ...</div>');
-                }
-            }).fail(function() {
+        $.ajax({
+            type: "GET",
+            url: 'https://sandbox.gibm.ch/tafel.php?klasse_id=' + klasse_id,
+            data: { format: 'json' }, // format und id mitgeben
+            dataType: 'json'
+        }).done(function(data) {
+            // wenn  Daten vorhanden sind ...
+            if (data !== '') {
+                // Tabelle generieren - Bootstrap Table
+                $('#stundenplan').append('<table class="table"><tr><th>Datum</th><th>Wochentag</th><th>Von</th><th>Bis</th><th>Lehrer</th><th>Fach</th><th>Longfach</th><th>Raum</th><th>Kommentar</th></tr></table>');
+                // Loop über JSON
+                $.each(data, function(key, tafel) {
+
+                    // Tabellenzeilen anfügen   
+                    $('#stundenplan table').append('<tr><td>' + tafel.tafel_datum +
+                        '</td><td>' + tafel.tafel_wochentag +
+                        '</td><td>' + tafel.tafel_von +
+                        '</td><td>' + tafel.tafel_bis +
+                        '</td><td>' + tafel.tafel_lehrer +
+                        '</td><td>' + tafel.tafel_fach +
+                        '</td><td>' + tafel.tafel_longfach +
+                        '</td><td>' + tafel.tafel_raum +
+                        '</td><td>' + tafel.tafel_kommentar +
+                        '</td></tr>');
+                })
+            } else {
                 // Fehlermeldung ausgeben - Bootstrap alert Box
-                $('#stundenplan').html('<div class="alert alert-danger">Fehler ... </div>');
-            });
+                $('#stundenplan').html('<div class="alert alert-warning">Wählen Sie eine Filiale aus ...</div>');
+            }
+        }).fail(function() {
+            // Fehlermeldung ausgeben - Bootstrap alert Box
+            $('#stundenplan').html('<div class="alert alert-danger">Fehler ... </div>');
         });
     };
 });
